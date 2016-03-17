7daaa43508b1a48cc9284f7cc56771aa3cfce2d5
Jelgar <jonathanelgardiner@gmail.com>
2016-03-17 23:50:00 +0000

    spec tweaks

--- spec/power_user_spec.rb
+++ spec/power_user_spec.rb
@@ -3,8 +3,10 @@ require_relative '../lib/power_user.rb'
 describe PowerUser do
   subject(:power_user) { described_class.new }
 
-  it 'should return a status of ACTIVE_USER for Joe Bloggs' do
-    expect(power_user.get_status('joebloggs@gmail.com')).to eq('ACTIVE_USER')
+  describe '#get_status' do
+    it 'should return a status of ACTIVE_USER for Joe Bloggs' do
+      expect(power_user.get_status('joebloggs@gmail.com')).to eq('ACTIVE_USER')
+    end
   end
 
 end
--- spec/scores_spec.rb
+++ spec/scores_spec.rb
@@ -5,8 +5,6 @@ describe Scores do
   let(:occurences) { {:analysis_performed=>10, :custom_audience_created=>3, :analysis_exported=>0, :report_downloaded=>7}
  }
 
-  # occurences =  {:analysis_performed=>10, :custom_audience_created=>3, :analysis_exported=>0, :report_downloaded=>7}
-
   it 'initializes with the current scores' do
     expect(scores.list).to include(:analysis_performed => 5)
   end
